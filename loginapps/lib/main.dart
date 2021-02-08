10fe823c651afe52c43cbf55dcbf586cc4052f3d
herry88 <herry081288@gmail.com>
2021-02-08 20:13:05 +0700

    AddSQL

--- loginapps/lib/main.dart
+++ loginapps/lib/main.dart
@@ -1,17 +1,16 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Project',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -21,126 +20,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //Deklarasikan Variabel Text
-
-  TextEditingController user = TextEditingController();
-  TextEditingController pass = TextEditingController();
-
-  //function 
-  Future login() async{
-    //config via url 
-    var url = "http://192.168.42.120/flutterbackend/login.php";
-    var response = await http.post(url, body:{
-        "username" : user.text,
-        "password" : pass.text
-    });
-    var data = json_decode(response.body);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Sign Up'),
-      ),
-      body: Container(
-        height: 300.0,
-        child: Card(
-          color: Colors.blueGrey,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(
-                      Icons.person,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        8.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: Icon(
-                      Icons.lock,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        8.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: MaterialButton(
-                      color: Colors.pink,
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {
-                        //login
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 5.0,
-                  ),
-                  Expanded(
-                    child: MaterialButton(
-                      color: Colors.pink,
-                      child: Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {
-                        //Register
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
