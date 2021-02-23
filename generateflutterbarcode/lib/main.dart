e64647323ab2506ac3d163728137853198c06ca0
herry88 <herry081288@gmail.com>
2021-02-23 14:12:43 +0700

    AddGenerate

--- generateflutterbarcode/lib/main.dart
+++ generateflutterbarcode/lib/main.dart
@@ -1,113 +1,236 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Uint8List bytes = Uint8List(0);
+  TextEditingController _inputController;
+  TextEditingController _outputController;
+
+  @override
+  initState() {
+    super.initState();
+    this._inputController = new TextEditingController();
+    this._outputController = new TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Builder(
+          builder: (BuildContext context) {
+            return ListView(
+              children: <Widget>[
+                _qrCodeWidget(this.bytes, context),
+                Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: this._inputController,
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.go,
+                        onSubmitted: (value) => _generateBarCode(value),
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.text_fields),
+                          helperText:
+                              'Please input your code to generage qrcode image.',
+                          hintText: 'Please Input Your Code',
+                          hintStyle: TextStyle(fontSize: 15),
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 7, vertical: 15),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: this._outputController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.wrap_text),
+                          helperText:
+                              'The barcode or qrcode you scan will be displayed in this area.',
+                          hintText:
+                              'The barcode or qrcode you scan will be displayed in this area.',
+                          hintStyle: TextStyle(fontSize: 15),
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 7, vertical: 15),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      this._buttonGroup(),
+                      SizedBox(height: 70),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () => _scanBytes(),
+        //   tooltip: 'Take a Photo',
+        //   child: const Icon(Icons.camera_alt),
+        // ),
       ),
-      home: MyHomePage(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Uint8List bytes = Uint8List(0);
-  TextEditingController text_1 = TextEditingController();
-  TextEditingController text_2 = TextEditingController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    this.text_1 = TextEditingController();
-    this.text_2 = TextEditingController();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Generate Barcode',
-        ),
-      ),
-      body: Center(
+  Widget _qrCodeWidget(Uint8List bytes, BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Card(
+        elevation: 6,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Please Input Your Code',
+          children: <Widget>[
+            Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Icon(Icons.verified_user, size: 18, color: Colors.green),
+                  Text('belajarflutter.com QrCode Generate', style: TextStyle(fontSize: 15)),
+                  Spacer(),
+                  Icon(Icons.more_vert, size: 18, color: Colors.black54),
+                ],
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 9),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4), topRight: Radius.circular(4)),
               ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Ouput Your Code',
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 40, right: 40, top: 30, bottom: 10),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 190,
+                    child: bytes.isEmpty
+                        ? Center(
+                            child: Text('Empty code ... ',
+                                style: TextStyle(color: Colors.black38)),
+                          )
+                        : Image.memory(bytes),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 7, left: 25, right: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 5,
+                          child: GestureDetector(
+                            child: Text(
+                              'remove',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.blue),
+                              textAlign: TextAlign.left,
+                            ),
+                            onTap: () =>
+                                this.setState(() => this.bytes = Uint8List(0)),
+                          ),
+                        ),
+                        Text('|',
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.black26)),
+                        // Expanded(
+                        //   flex: 5,
+                        //    child: GestureDetector(
+                        //     onTap: () async {
+                        //       final success =
+                        //           await ImageGallerySaver.saveImage(this.bytes);
+                        //       SnackBar snackBar;
+                        //       if (success) {
+                        //         snackBar = new SnackBar(
+                        //             content:
+                        //                 new Text('Successful Preservation!'));
+                        //         Scaffold.of(context).showSnackBar(snackBar);
+                        //       } else {
+                        //         snackBar = new SnackBar(
+                        //             content: new Text('Save failed!'));
+                        //       }
+                        //     },
+                        //     child: Text(
+                        //       'save',
+                        //       style:
+                        //           TextStyle(fontSize: 15, color: Colors.blue),
+                        //       textAlign: TextAlign.right,
+                        //     ),
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            this._button(),
+            Divider(height: 2, color: Colors.black26),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.history, size: 16, color: Colors.black38),
+                  Text('  Generate History',
+                      style: TextStyle(fontSize: 14, color: Colors.black38)),
+                  Spacer(),
+                  Icon(Icons.chevron_right, size: 16, color: Colors.black38),
+                ],
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 9),
+            )
           ],
         ),
       ),
     );
   }
 
-  Widget _button() {
-    //widget terpisah
+  Widget _buttonGroup() {
     return Row(
-      children: [
+      children: <Widget>[
         Expanded(
           flex: 1,
           child: SizedBox(
-            height: 120.0,
+            height: 120,
             child: InkWell(
+              onTap: () => _generateBarCode(this._inputController.text),
               child: Card(
                 child: Column(
-                  children: [
+                  children: <Widget>[
                     Expanded(
                       flex: 2,
-                      child: Image.asset(
-                        'images/generate_qrcode.png',
-                      ),
+                      child: Image.asset('images/generate_qrcode.png'),
                     ),
-                    Divider(
-                      height: 20.0,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text(
-                        'Generate',
-                      ),
-                    ),
+                    Divider(height: 20),
+                    Expanded(flex: 1, child: Text("Generate")),
                   ],
                 ),
               ),
             ),
           ),
         ),
+       
       ],
     );
+  }
+
+  
+
+  
+
+  Future _generateBarCode(String inputCode) async {
+    Uint8List result = await scanner.generateBarCode(inputCode);
+    this.setState(() => this.bytes = result);
   }
 }
